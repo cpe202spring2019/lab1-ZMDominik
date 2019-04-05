f6423beeffbac6d332774d1d7333259739ee9671
Zuzanna Dominik <zuzia.dominik@gmail.com>
2019-04-05 10:16:08 -0700

    inverse list

--- lab1.py
+++ lab1.py
@@ -1,15 +1,33 @@
 
 def max_list_iter(int_list):  # must use iteration not recursion
-   """finds the max of a list of numbers and returns the value (not the index)
-   If int_list is empty, returns None. If list is None, raises ValueError"""
-   pass
+    """finds the max of a list of numbers and returns the value (not the index)
+    If int_list is empty, returns None. If list is None, raises ValueError"""
+    if len(int_list) == 0:
+        return None
+    elif int_list is None:
+        raise ValueError
+    max_int = -99999
+    for num in int_list:
+        if num > max_int:
+            max_int = num
+    return max_int
+
 
 def reverse_rec(int_list):   # must use recursion
-   """recursively reverses a list of numbers and returns the reversed list
-   If list is None, raises ValueError"""
-   pass
+    """recursively reverses a list of numbers and returns the reversed list
+    If list is None, raises ValueError"""
+    if list is None:
+        return ValueError
+
+    if len(int_list) == 1:
+        return int_list
+    elif len(int_list) == 2:
+        return [int_list[-1], int_list[0]]
+    temp1 = [int_list[-1]]
+    temp2 = [int_list[0]]
+    return temp1+reverse_rec(int_list[1:-1])+temp2
 
 def bin_search(target, low, high, int_list):  # must use recursion
-   """searches for target in int_list[low..high] and returns index if found
-   If target is not found returns None. If list is None, raises ValueError """
-   pass
+    """searches for target in int_list[low..high] and returns index if found
+    If target is not found returns None. If list is None, raises ValueError """
+    pass
